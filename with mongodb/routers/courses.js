c6e5106e5939757cc9e76d090ee6e422e9a98ffc
nikita <freenikita95@gmail.com>
2020-06-02 00:01:41 +0300

    move working logic from file to mongo (Save, Edit, Show)

--- with mongodb/routers/courses.js
+++ with mongodb/routers/courses.js
@@ -3,7 +3,7 @@ const Course = require("../modules/course")
 const router = Router()
 
 router.get('/', async (req, res) => {
-    const courses = await Course.getAll()
+    const courses = await Course.find()
     res.render("courses", {
         title: 'Courses',
         isCourses: true,
@@ -12,8 +12,8 @@ router.get('/', async (req, res) => {
 })
 
 router.post('/edit', async (req, res) => {
-    Course.update(req.body)
-
+    const {id, ...fields} = req.body
+    await Course.findByIdAndUpdate(id, fields)
     res.redirect('/courses')
 })
 
@@ -21,9 +21,8 @@ router.get('/:id/edit', async (req, res) => {
     if(!req.query.allow ) {
         return res.redirect('/')
     }
-
-    const course = await Course.getById(req.params.id)
-
+    const {id} = req.params
+    const course = await Course.findById(id)
     res.render('course-edit',{
         title: `Edit ${course.title}`,
         course
@@ -31,7 +30,7 @@ router.get('/:id/edit', async (req, res) => {
 })
 
 router.get('/:id', async (req, res) => {
-    const course = await Course.getById(req.params.id)
+    const course = await Course.findById(req.params.id)
     res.render('course', {
         layout: 'empty',
         title: `Курс ${course.title}`,
