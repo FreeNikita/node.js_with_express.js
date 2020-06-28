fad43fe49495a772d9ca593f46adc4ef27582344
nikita <freenikita95@gmail.com>
2020-06-29 02:50:39 +0300

    move key to .env fiel

--- with mongodb/README.md
+++ with mongodb/README.md
@@ -4,3 +4,13 @@ You can start in console:
 
 * `npm install`
 * `npm run dev`
+
+
+example .env file:
+
+MONGO_URL=mongodb+srv://nikita:JaB5sqmHVoyLzfaT@cluster0-boop9.mongodb.net/shop
+PORT=process.env.PORT || 3000
+SESSION_SECRET=TEST,
+SENDGRID_API_KEY=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
+EMAIL_FROM=nikitanightwatchman@gmail.com
+BASE_URL=http://localhost:3000/
--- with mongodb/keys/index.js
+++ with mongodb/keys/index.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
+
 module.exports = {
-    MONGO_URL: "mongodb+srv://nikita:JaB5sqmHVoyLzfaT@cluster0-boop9.mongodb.net/shop",
-    PORT: process.env.PORT || 3000,
-    SESSION_SECRET: "TEST",
-    SENDGRID_API_KEY: "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",
-    EMAIL_FROM: 'nikitanightwatchman@gmail.com',
-    BASE_URL: 'http://localhost:3000/'
+    MONGO_URL: process.env.MONGO_URL,
+    PORT: process.env.PORT,
+    SESSION_SECRET: process.env.SESSION_SECRET,
+    SENDGRID_API_KEY: process.env.SENDGRID_API_KEY,
+    EMAIL_FROM: process.env.EMAIL_FROM,
+    BASE_URL: process.env.BASE_URL
 }
